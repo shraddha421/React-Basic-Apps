92aa8313e6f8cc6b8cb3d9eed4b5efdd4560b178
ShraddhaSalunkhe <sksalunkhe085@gmail.com>
2025-03-30 10:57:57 +0530

    Added variables and exported product component

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,18 +1,33 @@
 export function ProductCard() {
-    return (
-      <article>
-        <h2>iPhone 15 Pro</h2>
+  const Product={
+    imageSrc:"images/iphone.png",
+    title:"iPhone 15 Pro",
+    specification:[
+      "A17 Pro chip with 6-core GPU",
+      "3x or 5x Telephoto camera",
+      "Up to 29 hours video playback"
+    ],
+    price:999
+
+  } 
+  const listStyle={
+    listStyle:'none',padding:0
+  } 
+  
+  return (
+      <article style={{border: "1px solid white", padding:"16px", textAlign:"center"}}>
+        <h2>{Product.title}</h2>
         <img
-          src="images/iphone.png"
-          alt="iPhone 15 Pro"
+          src={Product.imageSrc}
+          alt={Product.title}
           width="128px"
           height="128px"
         />
         <p>Specification:</p>
-        <ul>
-          <li>A17 Pro chip with 6-core GPU</li>
-          <li>3x or 5x Telephoto camera</li>
-          <li>Up to 29 hours video playback</li>
+        <ul style={listStyle}>
+          <li>{Product.specification[0]}</li>
+          <li>{Product.specification[1]}</li>
+          <li>{Product.specification[2]}</li>
         </ul>
         <button>Buy (From $999)</button>
       </article>
