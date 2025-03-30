0af8603735c969b9095dcdbd506feb5544c7eca3
ShraddhaSalunkhe <sksalunkhe085@gmail.com>
2025-03-30 11:20:50 +0530

    passing props from parent to child component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import "./App.css";
 import { ProductCard } from "./components/ProductCard";
+import { ProductList } from "./components/ProductList";
 
 function App() {
-  return <div className="App"><ProductCard /></div>;
+  const product={
+    imageSrc:"images/iphone.png",
+    title:"iPhone 15 Pro",
+    specification:[
+      "A17 Pro chip with 6-core GPU",
+      "3x or 5x Telephoto camera",
+      "Up to 29 hours video playback"
+    ],
+    price:999
+
+  } 
+  return <div className="App">
+    <ProductList>
+    <ProductCard product={product} />
+    <ProductCard product={product} />
+    <ProductCard product={product} />
+    <ProductCard product={product} />
+
+    </ProductList>
+    </div>;
 }
 
 export default App;
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,33 +1,23 @@
-export function ProductCard() {
-  const Product={
-    imageSrc:"images/iphone.png",
-    title:"iPhone 15 Pro",
-    specification:[
-      "A17 Pro chip with 6-core GPU",
-      "3x or 5x Telephoto camera",
-      "Up to 29 hours video playback"
-    ],
-    price:999
-
-  } 
+export function ProductCard(props) {
+  
   const listStyle={
     listStyle:'none',padding:0
   } 
   
   return (
       <article style={{border: "1px solid white", padding:"16px", textAlign:"center"}}>
-        <h2>{Product.title}</h2>
+        <h2>{props.product.title}</h2>
         <img
-          src={Product.imageSrc}
-          alt={Product.title}
+          src={props.product.imageSrc}
+          alt={props.product.title}
           width="128px"
           height="128px"
         />
         <p>Specification:</p>
         <ul style={listStyle}>
-          <li>{Product.specification[0]}</li>
-          <li>{Product.specification[1]}</li>
-          <li>{Product.specification[2]}</li>
+          <li>{props.product.specification[0]}</li>
+          <li>{props.product.specification[1]}</li>
+          <li>{props.product.specification[2]}</li>
         </ul>
         <button>Buy (From $999)</button>
       </article>
