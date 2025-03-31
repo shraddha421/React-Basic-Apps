b06df910f1d27cb92525e8b11f002cf3952d1b49
ShraddhaSalunkhe <sksalunkhe085@gmail.com>
2025-03-31 12:55:45 +0530

    Conditional Rendering

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,21 +1,49 @@
-export function ProductCard({product, background="slategray",onPurchase,...restProps}) {
-  const listStyle={
-    listStyle:'none',padding:0
+export function ProductCard({
+  product,
+  background = "slategray",
+  onPurchase,
+  ...restProps
+}) {
+  const listStyle = {
+    listStyle: "none",
+    padding: 0,
+  };
+  function Status({ stockCount }) {
+    const notAvailableTemplate = (
+      <p style={{ fontSize: "14px", color: "lightsalmon" }}> Not Available</p>
+    );
+    const availableTemplate = (
+      <p style={{ fontSize: "14px", color: "lightgrey" }}>
+        {stockCount} items available
+      </p>
+    );
+    return stockCount === 0 ? notAvailableTemplate : availableTemplate;
   }
 
   return (
-      <article style={{border: "1px solid white", padding:"16px", textAlign:"center", background, width:"100%"}}>
-        <h2>{product.title}</h2>
-        <img
-          src={product.imageSrc}
-          alt={product.title}
-          {...restProps}
-        />
-        <p>Specification:</p>
-        <ul style={listStyle}>
-          {product.specification.map((spec)=>( <li key={spec}>{spec}</li>))}
-        </ul>
-        <button onClick={()=> onPurchase(product)}>Buy (From ${product.price})</button>
-      </article>
-    );
-  }
+    <article
+      style={{
+        border: "1px solid white",
+        padding: "16px",
+        textAlign: "center",
+        background,
+        width: "100%",
+      }}
+    >
+      <h2>{product.title}</h2>
+      <img src={product.imageSrc} alt={product.title} {...restProps} />
+      <p>Specification:</p>
+      <ul style={listStyle}>
+        {product.specification.map((spec) => (
+          <li key={spec}>{spec}</li>
+        ))}
+      </ul>
+      <Status stockCount={product.stockCount} />
+      {product.stockCount > 0 && (
+        <button onClick={() => onPurchase(product)}>
+          Buy (From ${product.price})
+        </button>
+      )}
+    </article>
+  );
+}
