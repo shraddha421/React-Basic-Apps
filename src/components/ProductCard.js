7fb8d54dd4364f51e76771aaa9e70098869a2540
ShraddhaSalunkhe <sksalunkhe085@gmail.com>
2025-04-05 10:10:08 +0530

    UseState hook usage

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,5 +1,5 @@
 import "../App.css";
-
+import { useState } from "react";
 export function ProductCard({
   product,
   background = "slategray",
@@ -10,6 +10,9 @@ export function ProductCard({
     listStyle: "none",
     padding: 0,
   };
+  const [stockCount, setStockount]=useState(product.stockCount)
+  const [showMore, setShowMore]=useState(false)
+  
   function Status({ stockCount }) {
     const notAvailableTemplate = (
       <p style={{ fontSize: "14px", color: "lightsalmon" }}> Not Available</p>
@@ -21,23 +24,39 @@ export function ProductCard({
     );
     return stockCount === 0 ? notAvailableTemplate : availableTemplate;
   }
+  function onHandleClick(product){
+    setStockount((prevstate)=> prevstate-1);
+    onPurchase(product);
+  }
+  function onShowMore(){
+    setShowMore(!showMore);
+  }
+  function onHandleTwoClicks(){
+    setStockount((prevstate)=> prevstate-1);
+    setStockount((prevstate)=> prevstate-1);
+  }
 
   return (
     <article className="Productcard" >
       <h2>{product.title}</h2>
       <img src={product.imageSrc} alt={product.title} {...restProps} />
       <p>Specification:</p>
-      <ul style={listStyle}>
+      <button onClick={onShowMore}>{showMore?"hide":"Show more"}</button>
+      {showMore && <ul style={listStyle}>
         {product.specification.map((spec) => (
           <li key={spec}>{spec}</li>
         ))}
-      </ul>
-      <Status stockCount={product.stockCount} />
-      {product.stockCount > 0 && (
-        <button onClick={() => onPurchase(product)}>
+      </ul>}
+      <Status stockCount={stockCount} />
+      {stockCount > 0 && (
+        <button onClick={() => onHandleClick(product)}>
           Buy (From ${product.price})
         </button>
+       
       )}
+       <button onClick={() => onHandleTwoClicks(product)}>
+          Buy 2
+        </button>
     </article>
   );
 }
